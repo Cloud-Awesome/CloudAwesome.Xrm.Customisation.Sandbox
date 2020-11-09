5504e655f547a06ef6454a16a150e1f028f0049d
Arthur <arthur@cloudawesome.uk>
2020-11-09 18:34:51 +0000

    Add EarlyBound entity model; Load and construct assembly record to be created; Add SamplePluginAssembly for testing;

--- CloudAwesome.Xrm.Customisation.Sandbox/Program.cs
+++ CloudAwesome.Xrm.Customisation.Sandbox/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Xml.Serialization;
-
+using CloudAwesome.Xrm.Customisation.Sandbox.EntityModel;
 using CloudAwesome.Xrm.Customisation.Sandbox.PluginModels;
+using Microsoft.Xrm.Sdk;
+using PluginAssembly = CloudAwesome.Xrm.Customisation.Sandbox.EntityModel.PluginAssembly;
 
 namespace CloudAwesome.Xrm.Customisation.Sandbox
 {
@@ -10,12 +13,11 @@ namespace CloudAwesome.Xrm.Customisation.Sandbox
     {
         static void Main(string[] args)
         {
-            
-            
+            Run();
             Console.ReadKey();
         }
 
-        public void Run()
+        public static void Run()
         {
             // 1. Get and Display the XML manifest
             const string sourceFile = "../../SampleManifest_v2.xml";
@@ -23,10 +25,32 @@ namespace CloudAwesome.Xrm.Customisation.Sandbox
 
             foreach (var pluginAssembly in manifest.PluginAssemblies)
             {
+                // 2. Register DLL
                 Console.WriteLine($"Assembly FriendlyName = {pluginAssembly.FriendlyName};");
 
+                var assemblyFileInfo = new FileInfo(pluginAssembly.Assembly);
+                var assembly = Assembly.LoadFile(assemblyFileInfo.FullName);
+                var version = assembly.GetName().Version.ToString();
+                var culture = assembly.GetName().CultureName;
+                var publicKeyToken = assembly.GetName().GetPublicKeyToken().ToString();
+                
+                PluginAssembly assemblyEntity = new PluginAssembly()
+                {
+                    Name = pluginAssembly.Name,
+                    Culture = culture,
+                    Version = version,
+                    PublicKeyToken = publicKeyToken,
+                    SourceType = PluginAssembly_SourceType.Database,// Only database supported now
+                    IsolationMode = PluginAssembly_IsolationMode.Sandbox, // Only Sandbox supported now
+                    Content = Convert.ToBase64String(File.ReadAllBytes(pluginAssembly.Assembly))
+                };
+
+                // TODO - Reference Core for Entity Extensions
+                // var assemblyId = assemblyEntity.Create();
+                
                 foreach (var plugin in pluginAssembly.Plugins)
                 {
+                    // 3. Register plugins
                     Console.WriteLine($"    PluginType FriendlyName = {plugin.FriendlyName}");
 
                     if (plugin.Steps == null)
@@ -36,24 +60,29 @@ namespace CloudAwesome.Xrm.Customisation.Sandbox
 
                     foreach (var step in plugin.Steps)
                     {
+                        // 4. Register plugin steps
                         Console.WriteLine($"        Step = {step.FriendlyName}");
                     }
                 }
             }
 
-            // 2. Register DLL
+            Console.ReadKey();
+
+            // 5. Register Service Endpoints
 
 
-            // 3. Register Steps
+
+            // 6. Register WebHooks
 
 
-            // 4. Register WF actions
+
+            // (Post-v1)
+            // (7. Register CWAs)
 
 
-            // 5. Add everything to specified solution
-
-
-            // 6. Remove anything unwanted? 
+            // (Maybe do this during each section above?)
+            // 8. Add everything to specified solution
+            // 9. Remove anything unwanted? 
             //      (Or clobber before registering?)
 
         }
---
+++ SamplePluginAssembly/UpdateContact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace SamplePluginAssembly
+{
+    public class UpdateContact: IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
