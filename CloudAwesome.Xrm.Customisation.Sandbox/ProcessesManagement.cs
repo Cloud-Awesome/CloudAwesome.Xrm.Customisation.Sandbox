5790d9efb410b5500fbd8bc062d9e4d513e5fe76
Arthur <arthur@cloudawesome.uk>
2020-11-17 12:44:55 +0000

    Initial Manifest for ProcessSetState feature

---
+++ CloudAwesome.Xrm.Customisation.Sandbox/ProcessesManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudAwesome.Xrm.Customisation.Sandbox
+{
+    public static class ProcessesManagement
+    {
+        public static void Activate()
+        {
+
+        }
+
+        public static void Deactivate()
+        {
+
+        }
+
+    }
+}
